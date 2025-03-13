4ae6a595401de411bc7fe94d2c626b794eba7593
ivanchenkoel7 <ivanchenkoel7@gmail.com>
2025-03-13 14:06:50 -0300

    nueva prueba en solucion de fallas

--- pfiv7/vite.config.js
+++ pfiv7/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Carpeta de salida para los archivos construidos
+    assetsInlineLimit: 0, // Asegúrate de que los activos no se inlinen, lo que permite que se manejen como archivos separados
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // Permite usar '@' para referirse a la carpeta src
+    },
+  },
+});
